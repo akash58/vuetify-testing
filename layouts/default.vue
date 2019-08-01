010aa5dc6b9ff44e6d3d9ebecd9d7aae4af7e600
akash58 <akashvishwakarma58@gmail.com>
2019-08-01 17:44:39 +0530

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-tile
+          router
+          :to="item.to"
+          :key="i"
+          v-for="(item, i) in items"
+          exact
+        >
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar fixed flat app class="primary">
+      <v-toolbar-side-icon @click="drawer = !drawer" class="white--text"></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title" class="white--text"></v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+      <nuxt />
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        drawer: false,
+        fixed: false,
+        items: [
+          { icon: 'apps', title: 'Welcome', to: '/' },
+          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' },
+          { icon: 'bubble_chart', title: 'Card', to: '/card' },
+          { icon: 'bubble_chart', title: 'Bottom', to: '/bottom' },
+          { icon: 'bubble_chart', title: 'Bottom Nav', to: '/bottom-nav' },
+          { icon: 'bubble_chart', title: 'Button', to: '/button' },
+          { icon: 'bubble_chart', title: 'Data Itertator', to: '/data-iterator' },
+          { icon: 'bubble_chart', title: 'Data Table', to: '/data-table' }
+        ],
+        title: 'Vuetify.js'
+      }
+    }
+  }
+</script>
---
+++ plugins/vuetify.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import Vuetify, {
+  VApp, // required
+  VNavigationDrawer,
+  VFooter,
+  VToolbar
+} from 'vuetify/lib'
+import { Ripple } from 'vuetify/lib/directives'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VToolbar
+  },
+  directives: {
+    Ripple
+  },
+  theme: {
+    primary: '#FF5722',
+    secondary: '#607D8B'
+  }
+})
